9ed36e208c4eef45be2111f27c9c96211227e1ce
younger <45989017+younger-1@users.noreply.github.com>
2021-03-02 15:19:40 +0800

    Redo 92

--- linked-list-medium/92.反转链表-ii.py
+++ linked-list-medium/92.反转链表-ii.py
@@ -10,10 +10,33 @@ class ListNode:
         self.val = val
         self.next = next
 
-    # @lc code=start
 
-
+# @lc code=start
 class Solution:
+    # 44/44 cases passed (56 ms)
+    # Your runtime beats 5.76 % of python3 submissions
+    # Your memory usage beats 20.37 % of python3 submissions (15.1 MB)
+    def reverseN(self, head: ListNode, N: int) -> ListNode:
+        if N == 1:
+            self.join = head.next
+            return head
+        last = self.reverseN(head.next, N - 1)
+        head.next.next = head
+        head.next = self.join
+        return last
+
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        if left == 1:
+            return self.reverseN(head, right)
+
+        head.next = self.reverseBetween(head.next, left - 1, right - 1)
+        return head
+
+
+# @lc code=end
+
+
+class Solution_1:
     # 执行用时： 56 ms , 在所有 Python3 提交中击败了 5.72% 的用户
     # 内存消耗： 14.9 MB , 在所有 Python3 提交中击败了 67.80% 的用户
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
@@ -33,6 +56,3 @@ class Solution:
         # Note: not return head:
         # eg.[3, 5], left=1, right=2, return head will cause reversed list to be [3]
         return dummy.next
-
-
-# @lc code=end
