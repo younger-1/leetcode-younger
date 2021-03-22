c8430305f81c0b49a9cd048ec2f189a54442297d
younger <45989017+younger-1@users.noreply.github.com>
2021-03-22 09:48:17 +0800

    [887] 鸡蛋掉落

--- heap-hard/887.鸡蛋掉落.py
+++ heap-hard/887.鸡蛋掉落.py
@@ -8,18 +8,48 @@
 class Solution:
     # 动态规划 + 二分查找
     def superEggDrop(self, k: int, n: int) -> int:
-        memo = dict()
-
-        def dp(eggs: int, floors: int) -> int:
-            pass
-
-        return dp(k, n)
+        pass
 
 
 # @lc code=end
 
+# 121/121 cases passed (2896 ms)
+# Your runtime beats 13.62 % of python3 submissions
+# Your memory usage beats 5.47 % of python3 submissions (44.3 MB)
+class Solution_2:
+    # 动态规划 + 二分查找
+    def superEggDrop(self, k: int, n: int) -> int:
+        memo = dict()
+
+        def dp(eggs: int, floors: int) -> int:
+            if eggs == 1:
+                return floors
+            if floors == 0:
+                return 0
+
+            if (eggs, floors) in memo:
+                return memo[eggs, floors]
+
+            res = floors
+            left, right = 1, floors
+            while left <= right:
+                mid = (left + right) // 2
+                broken = dp(eggs - 1, mid - 1)
+                not_broken = dp(eggs, floors - mid)
+                if broken > not_broken:
+                    right = mid - 1
+                    res = min(res, broken + 1)
+                else:
+                    left = mid + 1
+                    res = min(res, not_broken + 1)
+            memo[eggs, floors] = res
+            return res
+
+        return dp(k, n)
+
+
 # Time Limit Exceeded
-class Solution_0:
+class Solution_1:
     # 动态规划
     def superEggDrop(self, k: int, n: int) -> int:
         memo = dict()
