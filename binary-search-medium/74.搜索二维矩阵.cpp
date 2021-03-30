4fa107a3d5c0593489c95ae980fea69f9ba16e56
younger <45989017+younger-1@users.noreply.github.com>
2021-03-31 00:32:27 +0800

    [74] 搜索二维矩阵

---
+++ binary-search-medium/74.搜索二维矩阵.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=74 lang=cpp
+ *
+ * [74] 搜索二维矩阵
+ */
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+   public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2;
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = high + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
