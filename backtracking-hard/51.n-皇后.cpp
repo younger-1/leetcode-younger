ba99ae8c9f62c5769505a3d7b5655350ffa214e8
younger <45989017+younger-1@users.noreply.github.com>
2021-04-19 21:06:28 +0800

    [51] N 皇后

---
+++ backtracking-hard/51.n-皇后.cpp
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=cpp
+ *
+ * [51] N 皇后
+ */
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+
+// 9/9 cases passed (4 ms)
+// Your runtime beats 93.22 % of cpp submissions
+// Your memory usage beats 63.19 % of cpp submissions (7.3 MB)
+class Solution {
+   private:
+    vector<vector<string>> res;
+    // 路径：board 中小于 row 的那些行都已经成功放置了皇后
+    // 选择列表：第 row 行的所有列都是放置皇后的选择
+    // 结束条件：row 超过 board 的最后一行
+    void backtrace(vector<string>& board, int row) {
+        if (row == board.size()) {
+            res.push_back(board);
+            return;
+        }
+        int n = board[row].size();
+        for (int col = 0; col < n; col++) {
+            if (!isValid(board, row, col)) {
+                continue;
+            }
+            board[row][col] = 'Q';
+            backtrace(board, row + 1);
+            board[row][col] = '.';
+        }
+    }
+    /* 是否可以在 board[row][col] 放置皇后？ */
+    bool isValid(vector<string>& board, int row, int col) {
+        int n = board.size();
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+   public:
+    vector<vector<string>> solveNQueens(int n) {
+        // '.' 表示空，'Q' 表示皇后，初始化空棋盘。
+        vector<string> board(n, string(n, '.'));
+        backtrace(board, 0);
+        return res;
+    }
+};
+// @lc code=end
