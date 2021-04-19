a0c21b8d57b2b5efbd4dbeb1011bbc429ea85c6d
younger <45989017+younger-1@users.noreply.github.com>
2021-04-19 23:53:38 +0800

    [213] 打家劫舍 II

--- dynamic-programming-medium/213.打家劫舍-ii.cpp
+++ dynamic-programming-medium/213.打家劫舍-ii.cpp
@@ -33,3 +33,28 @@ class Solution {
     }
 };
 // @lc code=end
+
+class Solution_1 {
+   public:
+    int robRange(vector<int>& nums, int start, int end) {
+        // dp[i]=max(dp[i−2]+nums[i],dp[i−1])
+        // 滚动数组边界条件：dp[start]=nums[start], dp[start+1]=max(nums[start],nums[start+1])
+        int first = nums[start], second = max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+    int rob(vector<int>& nums) {
+        int len = nums.size();
+        if (len == 1) {
+            return nums[0];
+        } else if (len == 2) {
+            return max(nums[0], nums[1]);
+        }
+        return max(robRange(nums, 0, len - 2), robRange(nums, 1, len - 1));
+    }
+};
