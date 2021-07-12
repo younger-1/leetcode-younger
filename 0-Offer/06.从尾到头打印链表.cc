751049111d5068e5bef32aa48e0d7b18201e1ffb
younger <45989017+younger-1@users.noreply.github.com>
2021-07-12 16:33:42 +0800

    06.从尾到头打印链表

---
+++ 0-Offer/06.从尾到头打印链表.cc
@@ -0,0 +1,40 @@
+// 剑指 Offer 06. 从尾到头打印链表
+// https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
+
+#include <vector>
+using namespace std;
+
+/*
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// @recursion
+class Solution {
+   public:
+    vector<int> reversePrint(ListNode* head) {
+        if (!head)
+            return {};
+        vector<int> a = reversePrint(head->next);
+        a.push_back(head->val);
+        return a;
+    }
+};
+
+// @iteration
+class Solution_0 {
+   public:
+    vector<int> reversePrint(ListNode* head) {
+        vector<int> res;
+        ListNode* cur = head;
+        while (cur) {
+            res.insert(res.begin(), cur->val);
+            cur = cur->next;
+        }
+        return res;
+    }
+};
