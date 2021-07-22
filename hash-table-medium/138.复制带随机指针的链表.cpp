08a9c032c48e020c83caa8c6baaca98ef22ac748
younger <45989017+younger-1@users.noreply.github.com>
2021-07-22 16:07:35 +0800

    [138] 复制带随机指针的链表

---
+++ hash-table-medium/138.复制带随机指针的链表.cpp
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=138 lang=cpp
+ *
+ * [138] 复制带随机指针的链表
+ */
+
+class Node {
+   public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = nullptr;
+        random = nullptr;
+    }
+};
+
+// @lc code=start
+
+#include <unordered_map>
+
+using namespace std;
+
+class Solution_1 {
+   public:
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) {
+            return head;
+        }
+        unordered_map<Node*, Node*> map;
+        Node* cur = head;
+        while (cur) {
+            map[cur] = new Node(cur->val);
+            cur = cur->next;
+        }
+        cur = head;
+        while (cur) {
+            map[cur]->next = map[cur->next];
+            map[cur]->random = map[cur->random];
+            cur = cur->next;
+        }
+        return map[head];
+    }
+};
+// @lc code=end
+
+class Solution_2 {
+   public:
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) {
+            return head;
+        }
+        Node* cur = head;
+        while (cur) {
+            Node* tmp = new Node(cur->val);
+            tmp->next = cur->next;
+            cur->next = tmp;
+            cur = cur->next;
+        }
+        cur = head;
+        while (cur) {
+            if (cur->random) {
+                cur->next->random = cur->random->next;
+            }
+            cur = cur->next->next;
+        }
+        cur = head->next;
+        Node *pre = head, *res = cur;
+        while (cur) {
+            pre->next = pre->next->next;
+            cur->next = cur->next->next;
+            pre = pre->next;
+            cur = cur->next;
+        }
+        return res;
+    }
+};
