bd063867d0e2d685a9f36870d91e3ee9fca1cd14
younger <45989017+younger-1@users.noreply.github.com>
2021-07-24 14:36:21 +0800

    [560] 和为K的子数组

---
+++ hash-table-medium/560.和为k的子数组.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=560 lang=cpp
+ *
+ * [560] 和为K的子数组
+ */
+
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+   public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> preSum;
+        int sum = 0, res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            res += preSum[sum - k];
+            preSum[sum]++;
+        }
+        return res;
+    }
+};
+// @lc code=end
