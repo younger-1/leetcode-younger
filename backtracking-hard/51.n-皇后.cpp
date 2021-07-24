c56ba28baddbb27cecdaecdf2d87441b4956b2f9
younger <45989017+younger-1@users.noreply.github.com>
2021-07-24 22:08:19 +0800

    回溯

--- backtracking-hard/51.n-皇后.cpp
+++ backtracking-hard/51.n-皇后.cpp
@@ -43,19 +43,22 @@ class Solution {
             board[row][col] = '.';
         }
     }
-    /* 是否可以在 board[row][col] 放置皇后？ */
+    // 是否可以在 board[row][col] 放置皇后
     bool isValid(vector<string>& board, int row, int col) {
         int n = board.size();
+        // 正上一列
         for (int i = 0; i < row; i++) {
             if (board[i][col] == 'Q') {
                 return false;
             }
         }
+        // 左上对角线
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j] == 'Q') {
                 return false;
             }
         }
+        // 右上对角线
         for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
             if (board[i][j] == 'Q') {
                 return false;
--- backtracking-medium/46.全排列.cpp
+++ backtracking-medium/46.全排列.cpp
@@ -7,6 +7,26 @@
 
 using namespace std;
 
+/*
+ * 回溯算法框架。解决一个回溯问题，实际上就是一个决策树的遍历过程:
+ * 1、路径：也就是已经做出的选择。
+ * 2、选择列表：也就是你当前可以做的选择。
+ * 3、结束条件：也就是到达决策树底层，无法再做选择的条件。
+
+```python
+result = []
+def backtrack(路径, 选择列表):
+    if 满足结束条件:
+        result.add(路径)
+        return
+
+    for 选择 in 选择列表:
+        做选择
+        backtrack(路径, 选择列表)
+        撤销选择
+```
+ */
+
 // @lc code=start
 
 // 25/25 cases passed (4 ms)
