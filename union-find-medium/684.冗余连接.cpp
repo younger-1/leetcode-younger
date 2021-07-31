a353c7d628c74c5b0ac4f3afbec97f0a6a95fe9e
younger <45989017+younger-1@users.noreply.github.com>
2021-07-31 11:29:21 +0800

    [684] 冗余连接

--- union-find-medium/684.冗余连接.cpp
+++ union-find-medium/684.冗余连接.cpp
@@ -9,8 +9,39 @@
 using namespace std;
 
 // @lc code=start
+class Solution {
+   private:
+    vector<int> parent;
 
+   public:
+    int Find(int idx) {
+        if (parent[idx] != idx) {
+            parent[idx] = Find(parent[idx]);
+        }
+        return parent[idx];
+    }
+
+    void Union(int a, int b) { parent[Find(a)] = Find(b); }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        parent.resize(n + 1);
+        for (int i = 1; i <= n; ++i) {
+            parent[i] = i;
+        }
+        for (auto& edge : edges) {
+            int node1 = edge[0], node2 = edge[1];
+            if (Find(node1) != Find(node2)) {
+                Union(node1, node2);
+            } else {
+                return edge;
+            }
+        }
+        return vector<int>{};
+    }
+};
 // @lc code=end
+
 #include <numeric>
 
 class UnionFind {
@@ -18,7 +49,10 @@ class UnionFind {
     vector<int> id;
 
    public:
-    UnionFind(int n) : id(n) { iota(id.begin(), id.end(), 0); }
+    UnionFind(int n) : id(n) {
+        // iota函数可以把数组初始化为0到n-1
+        iota(id.begin(), id.end(), 0);
+    }
     int find(int x) {
         if (x != id[x]) {
             id[x] = find(id[x]);
