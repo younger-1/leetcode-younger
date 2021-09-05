e5f0ddb0a20331d4d8412f06fab89f73102b5e12
Xavier Young <45989017+younger-1@users.noreply.github.com>
2021-09-05 22:39:44 +0800

    [improve]

--- 0-OJ/tx2.cc
+++ 0-OJ/tx2.cc
@@ -1,36 +1,38 @@
-
-
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-vector<int> cache((int)1e5 + 1);
+vector<int> cache(1e5 + 1);
+vector<vector<pair<int, int>>> nums(1e5 + 1, vector<pair<int, int>>());
 
 // 所有可能的质因子
 int son(int n) {
-    if (cache[n]) {
-        return cache[n];
-    }
-    vector<int> p;
+    int nn = n;
     int i = 2;
     while (n != 1) {
         int k = 0;
+        if (cache[n]) {
+            nums[nn].reserve(nums[nn].size() + nums[n].size());
+            nums[nn].insert(nums[nn].end(), nums[n].begin(), nums[n].end());
+        }
         while (n % i == 0) {
             k++;
             n /= i;
         }
+        if (k) {
+            nums[nn].push_back({i, k + 1});
+        }
         i++;
-        p.push_back(k + 1);
     }
 
     int res = 1;
-    for (int i : p) {
+    for (auto& [_, i] : nums[nn]) {
         res *= i;
     }
     // cout << res << ",";
-    cache[n] = res;
+    cache[nn] = res;
     return res;
 }
 
