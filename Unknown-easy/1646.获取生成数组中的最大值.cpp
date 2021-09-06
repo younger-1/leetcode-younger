29aa4a7bca54cd93d716986041f0cf6ec74aed11
Xavier Young <45989017+younger-1@users.noreply.github.com>
2021-09-06 20:50:16 +0800

    [1646] 获取生成数组中的最大值

---
+++ Unknown-easy/1646.获取生成数组中的最大值.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1646 lang=cpp
+ *
+ * [1646] 获取生成数组中的最大值
+ */
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+   public:
+    int getMaximumGenerated(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        vector<int> nums(n + 1);
+        nums[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            nums[i] = nums[i / 2] + i % 2 * nums[i / 2 + 1];
+        }
+        return *max_element(nums.begin(), nums.end());
+    }
+};
+// @lc code=end
